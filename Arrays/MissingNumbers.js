4f3ab1a0c03af1b821922998d1b6c5c102cb819e
EdChiu-tech <edward_chiu604@gmail.com>
2021-08-11 12:30:14 -0700

    secondary solution

--- Arrays/MissingNumbers.js
+++ Arrays/MissingNumbers.js
@@ -23,36 +23,36 @@
 
 // Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
 
-nums = [1, 1]
+nums = [1,1]
 
-// const findDisappearedNumbers = (nums) => { 
-//     let sorted =  [...new Set(nums)].sort((a,b) => a-b)
-//     let counter = 0
-//     let missing = []
-//     for (i = 0; i < sorted.length; i++){
-//         counter++
-//         console.log(counter,sorted[i])
-//         if(sorted[i] !== counter){
-//             missing.push(counter)
-//         } 
-//     }
-//     return missing
-// };
-
-const findDisappearedNumbers = (nums) => {
-    let range = nums.length
-    let newArr = []
+const findDisappearedNumbers = (nums) => { 
+    let sorted =  nums.sort((a,b) => a-b)
+    let counter = 0
     let missing = []
-    for (let i = 1; i < range + 1; i++) {
-        newArr.push(i)
-        console.log(newArr)
+    for (i = 0; i < sorted.length; i++){
+        counter++
+        console.log(counter,sorted[i])
+        if(!sorted.includes(counter)){
+            missing.push(counter)
+        } 
     }
-    newArr.map(item => {
-        if (!nums.includes(item)) {
-            missing.push(item)
-        }
-    })
     return missing
 };
+
+// const findDisappearedNumbers = (nums) => {
+//     let range = nums.length
+//     let newArr = []
+//     let missing = []
+//     for (let i = 1; i < range + 1; i++) {
+//         newArr.push(i)
+//         console.log(newArr)
+//     }
+//     newArr.map(item => {
+//         if (!nums.includes(item)) {
+//             missing.push(item)
+//         }
+//     })
+//     return missing
+// };
 
 console.log(findDisappearedNumbers(nums))

8bd878c771167328dca0723360f2d87cf9cd1ee5
EdChiu-tech <edward_chiu604@gmail.com>
2021-08-24 12:47:47 -0700

    added refernce link

--- LinkedList/RemoveNthNodeFromEndOfList.js
+++ LinkedList/RemoveNthNodeFromEndOfList.js
@@ -2,7 +2,7 @@
 
 // Solution
 // Given the head of a linked list, remove the nth node from the end of the list and return its head.
-
+// https://redquark.org/leetcode/0019-remove-nth-node/
 
 // Example 1:
 
